7cdc5d3289b7f7603dd05ab5a490d49f71173228
Tomas <tomas.korzusehec@gm2dev.com>
2025-09-10 19:53:29 -0300

    feat(Header): add divider and stack layout to header component

--- src/modules/shared/components/Header/Header.component.tsx
+++ src/modules/shared/components/Header/Header.component.tsx
@@ -1,11 +1,16 @@
 import TopBanner from "./TopBanner/TopBanner.component";
 import Navbar from "./Navbar/Navbar.component";
+import { Divider, Stack } from "@mui/material";
+import { maxContentWidth } from "../../constants/pixels";
 
 export default function Header() {
   return (
     <header>
       <TopBanner />
       <Navbar />
+      <Stack sx={{ width: "100%", maxWidth: maxContentWidth, mx: "auto" }}>
+        <Divider sx={{ my: 0, borderWidth: 1, borderColor: "#00000010" }} />
+      </Stack>
     </header>
   );
 }
